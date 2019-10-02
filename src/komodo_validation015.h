7252d79cdc5da73615f730106f9c9be510112a43
phm87 <31578435+phm87@users.noreply.github.com>
2019-10-02 21:45:38 +0200

    Update komodo_validation015.h

--- src/komodo_validation015.h
+++ src/komodo_validation015.h
@@ -65,7 +65,7 @@
 #define portable_mutex_unlock pthread_mutex_unlock
 
 #define CRYPTO777_PUBSECPSTR "020e46e79a2a8d12b9b5d12c7a91adb4e454edfae43c0a0cb805427d2ac7613fd9"
-#define KOMODO_MINRATIFY 11
+#define KOMODO_MINRATIFY 3
 #define KOMODO_ELECTION_GAP 2000
 #define KOMODO_ASSETCHAIN_MAXLEN 65
 
@@ -217,6 +217,12 @@ static const char *notaries_elected[NUM_KMD_SEASONS][NUM_KMD_NOTARIES][2] =
         {"xrobesx_NA", "03f0cc6d142d14a40937f12dbd99dbd9021328f45759e26f1877f2a838876709e1" },
     },
     {
+        {"metaphilibert", "0275451a35b50b27bfa4935fe5af2015ac8a95f748036fa2c4db547565f105e5c2" },
+        {"phm87_SH", "02d8e32a6e677f862ea6d52006ab8fc2a72c96b7d7eff4921db1c58714a2339dbe" },
+        {"phm87_EU1", "03ad4e1317c982f11f05f41a6aacb244f28612dd792deefa68db740c0084588d49" },
+        {"phm87_EU2", "02a55c4d70b89403703e70ebb47f0cbb6a0183586d519e4f48e46ba4174be58d94" },
+        {"CrisF", "035b05d24f7a6062abcc096c618b154a58f326771ccf29b813191f68cceff0b864" },
+        /*
         {"madmax_NA", "02ef81a360411adf71184ff04d0c5793fc41fd1d7155a28dd909f21f35f4883ac1" },
         {"alright_AR", "036a6bca1c2a8166f79fa8a979662892742346cc972b432f8e61950a358d705517" },
         {"strob_NA", "02049202f3872877e81035549f6f3a0f868d0ad1c9b0e0d2b48b1f30324255d26d" },
@@ -281,6 +287,7 @@ static const char *notaries_elected[NUM_KMD_SEASONS][NUM_KMD_NOTARIES][2] =
         {"gt_AR", "0307c1cf89bd8ed4db1b09a0a98cf5f746fc77df3803ecc8611cf9455ec0ce6960" },
         {"patchkez_SH", "03d7c187689bf829ca076a30bbf36d2e67bb74e16a3290d8a55df21d6cb15c80c1" },
         {"decker_AR", "02a85540db8d41c7e60bf0d33d1364b4151cad883dd032878ea4c037f67b769635" },
+        */
     }
 };
 
